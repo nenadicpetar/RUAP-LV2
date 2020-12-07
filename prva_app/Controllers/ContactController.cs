413489a1e7533c7f3356a0dc18e1b7117be1dc08
nenadicpetar <nenadic.petar2@gmail.com>
2020-12-07 18:36:51 +0100

    Task 3

--- prva_app/Controllers/ContactController.cs
+++ prva_app/Controllers/ContactController.cs
@@ -1,37 +1,21 @@
-﻿using prva_app.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
 
 namespace prva_app.Controllers
 {
-    public class ContactController : Controller
+    public class ContactController : ApiController
     {
-        // GET: Contact
-        public ActionResult Index()
+        public string[] Get()
         {
-            return View();
-        }
-
-        public Contact[] Get()
-        {
-            return new Contact[]
-    {
-        new Contact
-        {
-            Id = 1,
-            Name = "Glenn Block"
-        },
-        new Contact
-        {
-            Id = 2,
-            Name = "Dan Roth"
-        }
-    };
+            return new string[]
+            {
+        "Hello",
+        "World"
+            };
         }
     }
-
-    
 }
