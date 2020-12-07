03773196c4e347e987b34fb1d390d3855bdafe2e
nenadicpetar <nenadic.petar2@gmail.com>
2020-12-07 19:16:36 +0100

    Exercise 3: Consume the Web API from an HTML Client

--- prva_app/Controllers/ContactController.cs
+++ prva_app/Controllers/ContactController.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using prva_app.Models;
 using prva_app.Services;
 
 namespace prva_app.Controllers
 {
+
     public class ContactController : ApiController
     {
+        
         private ContactRepository contactRepository;
 
         public ContactController()
@@ -20,6 +23,16 @@ namespace prva_app.Controllers
         {
             return contactRepository.GetAllContacts();
         }
+
+        public HttpResponseMessage Post(Contact contact)
+        {
+            this.contactRepository.SaveContact(contact);
+
+            var response = Request.CreateResponse<Contact>(System.Net.HttpStatusCode.Created, contact);
+
+            return response;
+        }
+
     }
 
 
